f11969f2f52c195bdbc3c1fd751d76fb7af3d5ea
EunJae <dldmswo1209@naver.com>
2022-09-08 22:58:31 +0900

    마이페이지 추가

--- app/src/main/java/com/dldmswo1209/dawnproject/myPage/MyPageFragment.kt
+++ app/src/main/java/com/dldmswo1209/dawnproject/myPage/MyPageFragment.kt
@@ -1,12 +1,25 @@
 package com.dldmswo1209.dawnproject.myPage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.dldmswo1209.dawnproject.R
+import com.dldmswo1209.dawnproject.databinding.FragmentMyPageBinding
+import com.dldmswo1209.dawnproject.login.LoginActivity
+import com.dldmswo1209.dawnproject.login.RegisterMainActivity
 
 class MyPageFragment : Fragment(R.layout.fragment_my_page) {
+    private lateinit var binding: FragmentMyPageBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMyPageBinding.bind(view)
+
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+        }
+        binding.registerButton.setOnClickListener {
+            startActivity(Intent(requireContext(), RegisterMainActivity::class.java))
+        }
     }
 }
